fd6b913099511e2059bd069cd130cb8264b9a5d1
Ian Wahbe <ian@wahbe.com>
2022-05-16 16:39:12 -0700

    Add a test

---
+++ sdk/yaml/bind/bind_test.go
@@ -0,0 +1,182 @@
+// Copyright 2022, Pulumi Corporation.  All rights reserved.
+
+package bind
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/blang/semver"
+	"github.com/hashicorp/hcl/v2"
+	yaml "github.com/pulumi/pulumi-yaml/pkg/pulumiyaml"
+	"github.com/pulumi/pulumi-yaml/pkg/pulumiyaml/ast"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/testing/utils"
+	"github.com/pulumi/pulumi/pkg/v3/resource/deploy/deploytest"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.lsp.dev/protocol"
+
+	"github.com/pulumi/pulumi-lsp/sdk/lsp"
+)
+
+const awsEksExample = `
+name: aws-eks
+runtime: yaml
+description: An EKS cluster
+variables:
+  vpcId:
+    Fn::Invoke:
+      Function: aws:ec2:getVpc
+      Arguments:
+        default: true
+      Return: id
+  subnetIds:
+    Fn::Invoke:
+      Function: aws:ec2:getSubnetIds
+      Arguments:
+        vpcId: ${vpcId}
+      Return: ids
+resources:
+  cluster:
+    type: eks:Cluster
+    properties:
+      vpcId: ${vpcId}
+      subnetIds: ${subnetIds}
+      instanceType: "t2.medium"
+      desiredCapacity: 2
+      minSize: 1
+      maxSize: 2
+outputs:
+  kubeconfig: ${cluster.kubeconfig}
+`
+
+func newDocument(name, body string) lsp.Document {
+	return lsp.NewDocument(protocol.TextDocumentItem{
+		URI:        protocol.DocumentURI("file://" + name),
+		LanguageID: protocol.YamlLanguage,
+		Text:       body,
+	})
+}
+
+func withLineAs(doc lsp.Document, lineNumber int, line string) {
+	existing, err := doc.Line(lineNumber)
+	if err != nil {
+		panic(err)
+	}
+	err = doc.AcceptChanges([]protocol.TextDocumentContentChangeEvent{{
+		Range: protocol.Range{
+			Start: protocol.Position{
+				Line:      uint32(lineNumber),
+				Character: 0,
+			},
+			End: protocol.Position{
+				Line:      uint32(lineNumber),
+				Character: uint32(len(existing)),
+			},
+		},
+		RangeLength: uint32(len(line)),
+		Text:        line,
+	}})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func cleanParse(t *testing.T, doc lsp.Document) *ast.TemplateDecl {
+	parsed, diags, err := yaml.LoadYAML(doc.URI().Filename(), strings.NewReader(doc.String()))
+	require.NoError(t, err)
+	assert.Len(t, diags, 0)
+	return parsed
+}
+
+// Create a simple range on a single line of ASCI text.
+func rangeOnLine(line, startByte, start, end int) *hcl.Range {
+	return &hcl.Range{
+		Start: hcl.Pos{
+			Line:   line,
+			Column: start,
+			Byte:   startByte,
+		},
+		End: hcl.Pos{
+			Line:   line,
+			Column: end,
+			Byte:   startByte + end - start,
+		},
+	}
+}
+
+func TestBind(t *testing.T) {
+	doc := newDocument("invalid-property", awsEksExample)
+	withLineAs(doc, 28, "  kubeconfig: ${cluster.kubeconfigg}")
+	parsed := cleanParse(t, doc)
+	decl, err := NewDecl(parsed)
+	require.NoError(t, err)
+	diags := decl.Diags()
+	require.Len(t, diags, 0)
+	decl.LoadSchema(rootPluginLoader)
+	diags = decl.Diags()
+	require.Len(t, diags, 1)
+	assert.Equal(t, &hcl.Diagnostic{
+		Severity: hcl.DiagError,
+		Summary:  "Property 'kubeconfigg' does not exist on eks:index:Cluster",
+		Detail:   "Existing properties are: kubeconfig, core, minSize, nodeAmiId, roleMappings, subnetIds, urn, userMappings, version, awsProvider, clusterTags, id, maxSize, name, provider, proxy, tags, vpcId, eksCluster, fargate, gpu, nodePublicKey, nodeSubnetIds, nodeUserData, publicSubnetIds, serviceRole, instanceRole, instanceRoles, instanceType, vpcCniOptions, desiredCapacity, nodeGroupOptions, publicAccessCidrs, storageClasses, defaultNodeGroup, privateSubnetIds, createOidcProvider, nodeRootVolumeSize, nodeSecurityGroup, useDefaultVpcCni, instanceProfileName, nodeRootVolumeIops, nodeRootVolumeType, clusterSecurityGroup, creationRoleProvider, eksClusterIngressRule, encryptionConfigKeyArn, endpointPublicAccess, nodeSecurityGroupTags, skipDefaultNodeGroup, clusterSecurityGroupTags, enabledClusterLogTypes, endpointPrivateAccess, providerCredentialOpts, encryptRootBlockDevice, nodeRootVolumeEncrypted, nodeRootVolumeThroughput, kubernetesServiceIpAddressRange, nodeAssociatePublicIpAddress, nodeRootVolumeDeleteOnTermination",
+		Subject:  rangeOnLine(29, 10, 25, 36),
+	}, diags[0])
+}
+
+func TestBindProperty(t *testing.T) {
+	doc := newDocument("property-binding", `
+variables:
+  binding: ${pulumi.foo}
+`)
+	parsed := cleanParse(t, doc)
+	decl, err := NewDecl(parsed)
+	require.NoError(t, err)
+	uses := decl.variables["pulumi"].uses
+	require.Len(t, uses, 1)
+	use := uses[0]
+	assert.Equal(t, &hcl.Range{
+		Start: hcl.Pos{
+			Line:   3,
+			Column: 12,
+		},
+		End: hcl.Pos{
+			Line:   3,
+			Column: 25,
+		},
+	}, use.Range())
+	assert.Equal(t, rangeOnLine(3, 9, 21, 24), use.access[0].rnge)
+}
+
+var defaultPlugins []yaml.Plugin = []yaml.Plugin{
+	{Package: "aws", Version: "4.26.0"},
+	{Package: "kubernetes", Version: "3.7.2"},
+	{Package: "eks", Version: "0.37.1"},
+
+	// Extra packages are to satisfy the versioning requirement of aws-eks.
+	// While the schemas are not the correct version, we rely on not
+	// depending on the difference between them.
+	{Package: "aws", Version: "4.15.0"},
+	{Package: "kubernetes", Version: "3.0.0"},
+}
+
+func newPluginLoader() schema.Loader {
+	schemaLoadPath := filepath.Join("..", "testdata")
+	host := func(pkg tokens.Package, version semver.Version) *deploytest.PluginLoader {
+		return deploytest.NewProviderLoader(pkg, version, func() (plugin.Provider, error) {
+			return utils.NewProviderLoader(pkg.String())(schemaLoadPath)
+		})
+	}
+	var pluginLoaders []*deploytest.PluginLoader
+	for _, p := range defaultPlugins {
+		pluginLoaders = append(pluginLoaders, host(tokens.Package(p.Package), semver.MustParse(p.Version)))
+	}
+
+	return schema.NewPluginLoader(deploytest.NewPluginHost(nil, nil, nil, pluginLoaders...))
+}
+
+var rootPluginLoader schema.Loader = newPluginLoader()
